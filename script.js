0b0c4abf8e51508cc4f9167fd1d19cb8f4756047
Isuru Shehan <157418313+isuru567@users.noreply.github.com>
2025-03-24 23:08:38 +0530

    Adding Motions

--- script.js
+++ script.js
@@ -2,6 +2,20 @@
 const menuBtn = document.getElementById("menu-btn");
 const navLinks = document.getElementById("nav-links");
 
+
+
 menuBtn.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
+
+
+//Adding motions
+
+const {animate}=Motion; 
+
+const element= document.querySelector(".hero-coffee");
+
+animate(
+
+    
+);
